7ea83ce324fb5b4f66741052e9063c0828b6b331
Julizzy <juliana.espinel.cortes@gmail.com>
2024-09-16 22:44:54 -0500

    actualizar page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
+"use client";
 import Header from "@/components/ui/Header";
 import Background from "@/components/ui/Background";
 import { Button } from "@/components/ui/Button";
 import Navbar from "@/components/ui/Navbar";
+import CarouselWithImages from "@/components/ui/CarouselWithImages";
+
 
 
 
 
 export default function Home() {
   return (
-    <Background >
-      <div>
-      </div>
+    <Background>
       <div className="relative w-full h-100">
         <Header />
         <Navbar />
         <main className="flex flex-col items-center justify-center min-h-screen">
-          <p className="mt-4 text-lg text-[#90bd82]">Gestiona los animales y cuida su bienestar.</p>
-          <Button className="px-8 py-6 bg-[#275255] text-white font-bold rounded">
-             Empezar
-          </Button>
+        <p className="text-2xl font-bold text-[#d5d897]">Gestiona los animales y cuida su bienestar.</p>
+          <div className="w-full max-w-4xl mt-8">
+            <CarouselWithImages />  
+          </div>
         </main>
-      </div> 
-      </Background>
+      </div>
+    </Background>
   );
 }
